7194f21b4691acd78052380983afd62239661ba0
Duzie Uche-Abba <72194611+duzzisantos@users.noreply.github.com>
2022-01-13 16:00:59 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Navigator } from "./components";
+import {Container} from './components';
+import { React } from "react";
+
+function App() {
+  return (
+    <>
+      <Navigator />
+      <Container/>
+    </>
+  );
+}
+
+export default App;
