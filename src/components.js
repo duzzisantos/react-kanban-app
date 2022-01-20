79df05958eb0abbafc8866e01ae48b70bafb1ea2
Duzie Uche-Abba <72194611+duzzisantos@users.noreply.github.com>
2022-01-20 22:48:55 +0100

    Update components.js

--- src/components.js
+++ src/components.js
@@ -1,6 +1,13 @@
 import { React, useState } from "react";
 import "./App.css";
-// import { ChangeTheme } from "./themes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faSun, faMoon, faBars } from "@fortawesome/free-solid-svg-icons";
+import { handleScheduledMenu } from "./1-menu-options";
+import { handleDoingMenu } from "./2-menu-options";
+import { handleDoneMenu } from "./3-menu-options";
+import { handleAddNewBoard } from "./newboard";
+import shortid from "shortid";
+import { Signup, Login, CreateNew, MyProjects, NightDayMode } from "./navlinks";
 
 export const Navigator = () => {
   //The reusable navbar component
@@ -8,27 +15,15 @@ export const Navigator = () => {
     <nav>
       <input type="checkbox" id="check"></input>
       <label htmlFor="check" className="checkbtn">
-        ==
+      <FontAwesomeIcon icon={faBars}/>
       </label>
       <label className="logo">Kanban</label>
       <ul className="nav-links">
-        <li>
-          <a href="https://www.jpmorgan.com">Sign up</a>
-        </li>
-        <li>
-          <a href="https://www.mtnonline.com">Login</a>
-        </li>
-        <li>
-          <a href="https://www.quora.com">Create new</a>
-        </li>
-        <li>
-          <a href="https://www.reddit.com/r/football">My projects</a>
-        </li>
-        <li>
-          <a href="https://www.google.com">
-            <ChangeTheme />
-          </a>
-        </li>
+       <Signup/>
+       <Login/>
+       <CreateNew/>
+       <MyProjects/>
+       <NightDayMode/>
       </ul>
     </nav>
   );
@@ -41,58 +36,146 @@ export const Container = () => {
       <ScheduledTasks />
       <DoingTasks />
       <DoneTasks />
-      <ExpandList />
+      <ExpandButton />
     </div>
   );
 };
+
+//Global declaration of our divs and delete button
 
 const ScheduledTasks = () => {
   //App function htmlFor scheduled tasks
   const [scheduled, setScheduled] = useState([]);
 
   const handleScheduledTasks = (event) => {
-    //event listener for adding new items on the anban board
+    //event listener for adding new items on the kanban board
     event.preventDefault();
-    setScheduled((task) => [...task, `${task.length + 1}.`]);
-    const adjustableBody = document.getElementById("root"); //needs modification
-    adjustableBody.scrollTop = adjustableBody.scrollHeight;
+    setScheduled((task) => [...task, task.length.toString]); // key={shortid.generate}
   };
 
-  const handleRemoveTasks = (event) => {
-    //event listener for clearing items on the kanban board
-    event.preventDefault();
-    const newSchedule = scheduled.filter(
-      (schedule) => schedule.lastElementChild
-    );
-    alert("Are you sure you want to clear items?"); //add a dialog box here instead of an alert.
-    setScheduled(newSchedule);
-    console.log("Schedule has been cleared.");
+  const handleCardDelete = () => {
+    const scheduler = document.getElementById("scheduled");
+    const doing = document.getElementById("doing");
+    const done = document.getElementById("done");
+    const addBoard = document.getElementById("add-board");
+    const cardDeletes = document.querySelectorAll(".card-delete-btn");
+    cardDeletes.id = shortid.generate();
+    const textareaWrap = document.querySelectorAll(".textarea-wrap");
+    textareaWrap.id = shortid.generate();
+    for (let i = 0; i < textareaWrap.length; i++) {
+      for (let j = 0; j < cardDeletes.length; j++) {
+        cardDeletes[j].addEventListener("click", (e) => {
+          e.preventDefault();
+          if (scheduler.contains(cardDeletes[j])) {
+            scheduler.removeChild(textareaWrap[i]);
+          } else if (doing.contains(cardDeletes[j])) {
+            doing.removeChild(textareaWrap[i]);
+          } else if (done.contains(cardDeletes[j])) {
+            done.removeChild(textareaWrap[i]);
+          } else if (addBoard.contains(cardDeletes[j])) {
+            addBoard.removeChild(textareaWrap[i]);
+          }
+        });
+      }
+    }
   };
+
   return (
-    <div id="scheduled" onDrop={DropEvent} onDragOver={DragOver}>
-      <h4>To do</h4>
-      {scheduled.map((task) => (
-        <textarea id="textArea" draggable="true" spellCheck='false' onDragStart={DragStart}>
-          {task}
-        </textarea>
-      ))}
+    <div
+      id="main-wrapper1"
+      onDrop={DropEvent}
+      onDragOver={DragOver}
+      onDragEnter={DragEnter}
+    >
+      <div id="scheduled">
+        <div id="title-wrap1">
+          <input
+            type="text"
+            id="board-title1"
+            spellCheck="false"
+            placeholder="Title 1"
+            autoCorrect="on"
+            autoComplete="off"
+          ></input>
+          <button
+            type="button"
+            className="options-btn-scheduled"
+            onClick={handleScheduledMenu}
+          >
+            ...
+          </button>
+        </div>
+        {scheduled.map((task) => (
+          <div
+            className="textarea-wrap"
+            id={shortid.generate()}
+            draggable="true"
+            onDragStart={DragStart}
+          >
+            {task}
+            <textarea
+              className="textArea"
+              id={shortid.generate()}
+              spellCheck="false"
+              autoCorrect="on" //this is compatible with Safari Browser
+              autoComplete="off"
+            ></textarea>
+            <input type="date" className="timer-btn"></input>
+            <button type="button" className="card-save-btn">
+              Save
+            </button>
+            <button
+              type="button"
+              className="card-delete-btn"
+              onClick={handleCardDelete}
+              id={shortid.generate()}
+            >
 
-      <button type="button" id="btn-add" onClick={handleScheduledTasks}>
-        +
-      </button>
-      <button type="button" id="btn-remove" onClick={handleRemoveTasks}>
-        <span style={{ color: "orangered" }}>-</span>
-      </button>
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+          </div>
+        ))}
+
+        <button type="button" id="btn-add" onClick={handleScheduledTasks}>
+          +
+        </button>
+        {/* <button type="button" id="btn-remove" onClick={handleRemoveTasks}>
+          <span style={{ color: "orangered" }}>-</span>
+        </button> */}
+      </div>
     </div>
   );
 };
 
 const DoingTasks = () => {
-  //App function htmlFor tasks being done
+  //App function For tasks being done
   return (
-    <div id="doing" onDrop={DropEvent} onDragOver={DragOver} droppable='true'>
-      <h4>Doing</h4>
-      
+    <div
+      id="main-wrapper2"
+      onDrop={DropEvent}
+      onDragOver={DragOver}
+      onDragEnter={DragEnter}
+      onDragStart={DragStart}
+    >
+      <div id="doing">
+        <div id="title-wrap2">
+          <input
+            type="text"
+            id="board-title2"
+            spellCheck="false"
+            placeholder="Title 2"
+            autoCorrect="on"
+            autoComplete="off"
+          ></input>
+          <button
+            type="button"
+            className="options-btn-doing"
+            onClick={handleDoingMenu}
+          >
+            ...
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
@@ -100,70 +183,47 @@ const DoingTasks = () => {
 const DoneTasks = () => {
   //App function htmlFor done tasks
   return (
-    <div id="done" onDrop={DropEvent} onDragOver={DragOver}>
-      <h4>Done</h4>
-      
+    <div
+      id="main-wrapper3"
+      onDrop={DropEvent}
+      onDragOver={DragOver}
+      onDragEnter={DragEnter}
+      onDragStart={DragStart}
+    >
+      <div id="done">
+        <div id="title-wrap3">
+          <input
+            type="text"
+            id="board-title3"
+            spellCheck="false"
+            placeholder="Title 3"
+            autoCorrect="on"
+            autoComplete="off"
+          ></input>
+          <button
+            type="button"
+            className="options-btn-done"
+            onClick={handleDoneMenu}
+          >
+            ...
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-const ExpandList = () => {
-  //App function htmlFor adding extra tasks
-
-  const [expand, setExpand] = useState([]);
-  const [newAdd, setNewAdd] = useState([]);
-  const [newRemove, setNewRemove] = useState([]);
-
-  const handleExpandList = (event) => {
-    //event listener for creating more kanban boards
-    event.preventDefault();
-    setExpand((myExpand) => [...myExpand, myExpand.length.toString]);
-    setNewAdd((myNewAdd) => [...myNewAdd, myNewAdd.length.toString]);
-    setNewRemove((myNewRemove) => [
-      ...myNewRemove,
-      myNewRemove.length.toString,
-    ]);
-  };
-
-  // const handleNewAdd = () =>{
-
-  // }
-
-  // const handleNewRemove = () =>{
-  //    setNewRemove((myNewRemove) => [...myNewRemove, `${myNewRemove.innerHTML = ""}`])
-  // }
+const ExpandButton = () => {
   return (
-    <>
-      {expand.map((expansion) => (
-        <div
-          id="expand"
-          style={{ backgroundColor: "#EFF6E0" }}
-          onDrop={DropEvent}
-          onDragOver={DragOver}
-        >
-          {expansion}
-          {newAdd.map((newAdds) => (
-            <button id="new-add-btn">+{newAdds}</button>
-          ))}
-          {newRemove.map((newRemoves) => (
-            <button id="new-remove-btn">
-              {" "}
-              <span style={{ color: "orangered" }}>-</span>
-              {newRemoves}
-            </button>
-          ))}
-        </div>
-      ))}
-      <button type="button" id="btn-expand" onClick={handleExpandList}>
-        +
-      </button>
-    </>
+    <button type="button" id="btn-expand" onClick={handleAddNewBoard}>
+      +
+    </button>
   );
 };
 
-//Themes htmlFor day and night mode
+//THEMES FOR CHANGING TO NIGHTMODE AND DAYMODE
 
-const ChangeTheme = () => {
+export const ChangeTheme = () => {
   const [background, setBackground] = useState("");
   const [buttonColor, setButtonColor] = useState("");
 
@@ -173,10 +233,10 @@ const ChangeTheme = () => {
     const docBody = document.querySelector(".container");
     event.preventDefault();
     setBackground(() => {
-      docBody.style.backgroundColor = "black";
+      docBody.style.backgroundColor = "rgb(46,46,46)";
     });
     setButtonColor(() => {
-      nightBtn.style.backgroundColor = "rgb(56, 56, 56)";
+      nightBtn.style.backgroundColor = "transparent";
     });
   };
 
@@ -189,7 +249,7 @@ const ChangeTheme = () => {
       docBody.style.backgroundColor = "white";
     });
     setButtonColor(() => {
-      dayBtn.style.backgroundColor = "rgb(56, 56, 56)";
+      dayBtn.style.backgroundColor = "transparent";
     });
   };
 
@@ -198,39 +258,35 @@ const ChangeTheme = () => {
       {background}
       {buttonColor}
       <button type="button" id="night-mode-btn" onClick={handleNightMode}>
-        &#127769;
+        <FontAwesomeIcon icon={faMoon} />
       </button>
       <button type="button" id="day-mode-btn" onClick={handleDayMode}>
-        &#127774;
+        <FontAwesomeIcon icon={faSun} />
       </button>
     </>
   );
 };
 
-
-//Drag events 
-
-function DragStart(event) {
-  event.dataTransfer.setData('Text', event.target.id);
-  IncreaseBoardSize();
+//DRAG AND DROP LOGIC
+export function DragStart(event) {
+  event.dataTransfer.setData("Text", event.target.id);
 }
 
-function DragOver(event) {
+export function DragOver(event) {
   event.preventDefault();
 }
 
-function DropEvent(event) {
+export function DragEnter(event) {
   event.preventDefault();
-  var droppedData = event.dataTransfer.getData('Text');
-  event.target.appendChild(document.getElementById(droppedData)); //this gets the data via the ID of the element we are dragging
 }
 
-//increase board size during dragEvent
+export function DropEvent(event) {
+  event.preventDefault();
+  var droppedData = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(droppedData));
+  //this gets the data via the ID of the textarea (in #scheduled)
+}
 
-function IncreaseBoardSize(){
-  var increaseDoingSize = document.getElementById("doing");
-  increaseDoingSize.style.height = 'fit-content';
-  var increaseDoneSize = document.getElementById("done");
-  increaseDoneSize.style.height = 'fit-content';
-  console.log("drag started");
+export function DragLeave(event) {
+  event.preventDefault();
 }
